3d56b0ac8d480f54abbe2e79bf1cc7d98923a0bd
Ethan Sim <ethansimrm@gmail.com>
2023-06-26 00:30:53 +0100

    Incorporate NER into TUR

--- README.md
+++ README.md
@@ -11,8 +11,9 @@ The `data_processing` directory hosts the scripts I used to align primary data, 
 ## Implementation and Reproducibility
 All models have been trained on [Kaggle's](Kaggle.com) P1000 GPU, and are hosted on [my HuggingFace profile](https://huggingface.co/ethansimrm). All raw data files, and all scripts/notebooks used, will be hosted here. Note that in some scripts, some features, such as [Weights and Biases](https://wandb.ai/) for model training statistics, may be specific to Kaggle. 
 All data files are also uploaded on my HuggingFace profile. I process these into a convenient format for training (i.e., a `Dataset` object) in situ, due to issues with HuggingFace's programmatic upload.
+I use two virtual environments; the one described by `scispacey_requirements.txt` uses [Conda](https://www.anaconda.com/download) and runs Python 3.8 specifically for [SciSpacy](https://allenai.github.io/scispacy/). The other one runs Python 3.10.11 and uses a normal venv.   
 ## Current Progress
-I am currently finetuning a pre-trained English-to-French model from [OPUS-MT](https://huggingface.co/Helsinki-NLP/opus-mt-en-fr) with data provided by the WMT Biomedical Translation Task organisers. I've aligned and preprocessed my training data, and computed results from five metrics (BLEU, CHRF++, TER, METEOR, and two flavours of Terminology Usage Rate) over this model's predictions. 
+I am currently finetuning a pre-trained English-to-French model from [OPUS-MT](https://huggingface.co/Helsinki-NLP/opus-mt-en-fr) with data provided by the WMT Biomedical Translation Task organisers. I've aligned and preprocessed my training data, and computed results from five metrics (BLEU, CHRF++, TER, METEOR, and three flavours of Terminology Usage Rate) over this model's predictions. 
 ## TODO
 - Porting my code over to a more powerful GPU cluster, while still retaining compute consumption.
 - Implementing a better way to translate biomedical-domain information.
--- model_evaluations/test_results.py
+++ model_evaluations/test_results.py
@@ -27,39 +27,51 @@ def generate_tokens(pred_list):
     fr_tagged = []
     for sentence in tqdm(pred_list):
         fr_tagged_sent = fr_tagger(sentence)
-        fr_tagged_tokenised = [token.text.lower() for token in fr_tagged_sent] #We just need the text only, not the entire token object. 
-        #We will ignore casing errors, because we know the meaning is not altered.
+        fr_tagged_tokenised = [token.text for token in fr_tagged_sent] #We just need the text only, not the entire token object. 
         fr_tagged.append(fr_tagged_tokenised)
 
-#Rule-based checker which ignores casing errors
-def find_term_in_sentence(row): 
-    query = row["fr_term"] #Already in lowercase
-    total_occurrences = len([found for found in fr_tagged[row["sent_ID"]] if query == found]) #Account for more than one occurrence of the same term per sentence
+#Rule-based checker which optionally ignores casing errors and partitive article errors, depending on stringency.
+def find_term_in_sentence(row, stringency):
+    if (stringency == "strict"):
+        query = row["fr_term"]
+        total_occurrences = len([found for found in fr_tagged[row["sent_ID"]] if query == found])
+    elif (stringency == "caseless"):
+        query = row["fr_term"].lower()
+        total_occurrences = len([found for found in fr_tagged[row["sent_ID"]] if query.lower() == found.lower()])
+    elif (stringency == "loose"):
+        query = row["fr_term"].lower()
+        total_occurrences = len([found for found in fr_tagged[row["sent_ID"]] if (query.lower() == found.lower()) or 
+                                 ("d" + query.lower() == found.lower()) or 
+                                 ("l" + query.lower() == found.lower())])
+    else:
+        print("Invalid stringency!")
+        return
     if (total_occurrences <= row["counts"]): #Account for predicting some but not all
         return total_occurrences
     else: #Over-prediction should be ignored - other metrics will suffer if this is severe.
         return row["counts"]
-        
-#Looser rule-based checker which ignores partitive article errors as well, since these are also human-interpretable
-def find_term_in_sentence_loose(row): 
-    query = row["fr_term"]
-    total_occurrences = len([found for found in fr_tagged[row["sent_ID"]] if (query == found) or ("d" + query == found) or ("l" + query == found)])
-    if (total_occurrences <= row["counts"]):
-        return total_occurrences
-    else:
-        return row["counts"]
 
-#Terminology usage metric expressed as proportion of found terminologies for both checkers (depending on stringency)
+#Terminology usage metric expressed as proportion of found terminologies for all three stringency levels.
 def generate_TUR(pred_list, term_refs):
+    
+    #Check for terms
     generate_tokens(pred_list)
-    term_refs["matches"] = term_refs.apply(find_term_in_sentence, axis=1)
-    term_refs["matches_loose"] = term_refs.apply(find_term_in_sentence_loose, axis=1)
-    term_refs.to_csv("found_terminology.txt", sep="\t", index=False, header=False)
+    term_refs["matches_strict"] = term_refs.apply(find_term_in_sentence, args=("strict",), axis=1)
+    term_refs["matches_caseless"] = term_refs.apply(find_term_in_sentence, args=("caseless",), axis=1)
+    term_refs["matches_loose"] = term_refs.apply(find_term_in_sentence, args=("loose",), axis=1)
+    term_refs.to_csv("found_terminology.txt", sep="\t", index=False, header=True)
+    
+    #Compute statistics
     total = term_refs["counts"].sum()
-    non_cased_matches = term_refs["matches"].sum()
+    exact_matches = term_refs["matches_strict"].sum()
     print("Total Terminologies: ", total)
+    print("Exact Matches: ", exact_matches)
+    print({"tur": exact_matches/total })
+    
+    non_cased_matches = term_refs["matches_caseless"].sum()
     print("Non-cased Matches: ", non_cased_matches)
-    print({"tur": non_cased_matches/total })
+    print({"tur_caseless": non_cased_matches/total })
+    
     non_cased_matches_loose = term_refs["matches_loose"].sum()
     print("Non-cased Matches ignoring partitive article errors: ", non_cased_matches_loose)
     print({"tur_loose": non_cased_matches_loose/total})
